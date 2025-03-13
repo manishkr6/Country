db396cf1dc68f243c55722f4981f1c1db16d9d53
manish <manishkr9547@gmail.com>
2025-03-13 19:26:25 +0530

    country

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Country from "./pages/Country";
+import Contact from "./pages/Contact";
+import "./App.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/country", element: <Country /> },
+  { path: "/contact", element: <Contact /> },
+]);
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
