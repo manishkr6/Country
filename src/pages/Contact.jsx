0f7f4bbbd25e7f58c10ef95d454453f56bc76408
manish <manishkr9547@gmail.com>
2025-03-16 19:22:47 +0530

    Implement Contact page with form for user input and styling adjustments

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className=" flex gap-20 flex-col items-center">
+      <h2 className="contact-us-h2 text-6xl leading-tight">Contact Us</h2>
 
-export default Contact
+      <div className=" p-10 rounded-lg w-2xl shadow-lg">
+        <form className="flex flex-col gap-10 ">
+          <input
+            type="text"
+            className="border-2 border-gray-500 h-20 w-full rounded-[8px] placeholder:p-5  "
+            required
+            autoComplete="off"
+            placeholder="Enter your name"
+            name="username"
+          />
+          <input
+            type="email"
+            className="border-2 border-gray-500 h-20 w-full rounded-[8px] placeholder:p-5 "
+            required
+            autoComplete="off"
+            placeholder="Enter your email"
+            name="email"
+          />
+          <textarea
+            className="border-2 border-gray-500 h-65 w-full rounded-[8px] placeholder:p-5 "
+            rows="10"
+            required
+            autoComplete="off"
+            placeholder="Enter your message"
+            name="message"
+          ></textarea>
+
+          <button type="submit" className="contact-us-btn">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
